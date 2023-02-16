a7e7a69c9192f4a3cffc47cf9eccd1596b7ed870
HimatJutla <himatdeveloper@gmail.com>
2023-02-16 15:52:52 -0500

    removed getStatisProps from the new player page as it is no longer needed and removed unused imports

--- pages/player/new.tsx
+++ pages/player/new.tsx
@@ -1,14 +1,8 @@
-import EmptyState from '@/components/emptyState/emptyState';
 import HeadMetaData from '@/components/headMetaData/headMetaData';
-import MatchesForm from '@/components/matches/matches-form';
 import PlayerForm from '@/components/players/playerForm';
 import TennisMatchTrackerFooter from '@/components/ui/footer/tennis-match-tracker-footer';
 import TennisMatchTrackerHeader from '@/components/ui/header/tennis-match-tracker-header';
-import { mongoDbCLientConnectionUrl } from '@/consts/mongodb-client-url-connect';
-import { Match } from '@/interfaces/match/match.interface';
 import { Player } from '@/interfaces/player/player.interface';
-import { MatchPagesProps } from '@/interfaces/props/page-props/match-pages-props.interface';
-import { MongoClient } from 'mongodb';
 import router from 'next/router';
 
 export default function NewPlayerPage() {
@@ -42,38 +36,3 @@ export default function NewPlayerPage() {
     </>
   )
 }
-
-export async function getStaticProps() {
-
-    try {
-      const client = await MongoClient.connect(
-        mongoDbCLientConnectionUrl
-      );
-      const db = client.db();
-      const playersCollection = db.collection('players');
-      const players = await playersCollection.find().toArray();
-      client.close();
-      return {
-  
-        props: {
-          players: players.map((player) => ({
-            id: player._id.toString(),
-            firstName: player.firstName,
-            lastName: player.lastName,
-            bio: player.bio,
-            dateOfBirth: player.dateOfBirth,
-            wins: player?.wins ? player.wins : null,
-            losses: player?.losses ? player.losses : null,
-            image: player?.image ? player.image: null,
-            country: player.country,
-            city: player.city,
-            email: player.email
-          }))
-        },
-        revalidate: 60,
-      };
-    } catch(error) {
-      console.error(error);
-    }
-  }
-  
